7e5181e9db69b20aba10451d65dd30b66bde8100
Harsh Mahalwar <78298221+HarshMahalwar@users.noreply.github.com>
2022-06-01 23:23:02 +0530

    Update settings.py

--- lab_copy/settings.py
+++ lab_copy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@vuaflh&5ke60#^5bost^mt(ri3t)vpy5xg7xuuiug*cpu@t+r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'thisisarestfulapi.herokuapp.com/']
 
 
 # Application definition
